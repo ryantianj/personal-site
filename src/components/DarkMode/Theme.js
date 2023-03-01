8f13976556a6227e1d0d60c3c04b9b816bb7f4dd
ryantianj <ryantian64@gmail.com>
2023-03-01 12:38:31 +0800

    Save theme

--- src/components/DarkMode/Theme.js
+++ src/components/DarkMode/Theme.js
@@ -5,18 +5,27 @@ import {MdOutlineLightMode, MdDarkMode} from "react-icons/md";
 const LIGHT_MODE = 'light'
 const DARK_MODE = 'dark'
 const Theme = () => {
-    const [isLight, setIsLight] = useState(window.matchMedia(`(prefers-color-scheme: ${LIGHT_MODE}`).matches)
+    const isLightTheme = () => {
+        if (localStorage.getItem('data-theme')) {
+            return localStorage.getItem('data-theme') === LIGHT_MODE
+        }
+        return window.matchMedia(`(prefers-color-scheme: ${LIGHT_MODE}`).matches
+    }
+    const [isLight, setIsLight] = useState(isLightTheme())
 
-    const handleClick = (e) => {
+    const handleClick = () => {
         setIsLight(prevState => !prevState)
     }
 
     useEffect(() => {
         if (isLight) {
             document.documentElement.setAttribute('data-theme', LIGHT_MODE)
+            localStorage.setItem('data-theme', LIGHT_MODE)
         } else {
             document.documentElement.setAttribute('data-theme', DARK_MODE)
+            localStorage.setItem('data-theme', DARK_MODE)
         }
+
     }, [isLight])
 
     return (
