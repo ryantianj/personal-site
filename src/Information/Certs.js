440c2766fe152bc91e7952e1b6f3e037c7a93de3
ryantianj <ryantian64@gmail.com>
2023-03-07 16:01:43 +0800

    update cert

--- src/Information/Certs.js
+++ src/Information/Certs.js
@@ -1,6 +1,7 @@
 import udemyImage from "./images/Udemy.png"
 import orbitalImage from "./images/orbital.jfif"
 import mongoDBUniImage from "./images/mongodb university.jfif"
+import AISGImage from "/images/AISG.jpeg"
 export const certsInfo = [
     {
         url: "https://credentials.nus.edu.sg/ccdf6d1c-267a-4c47-a4a3-56623598d117",
@@ -25,5 +26,13 @@ export const certsInfo = [
         name: "MongoDB Basics",
         date: "May 2021",
         photo: mongoDBUniImage
+    },
+    {
+        url: "https://certified.aisingapore.org/b3e8bd29-fdba-4fbc-9800-ebda7d79a336?_ga=2.138831927.524651342.1678175243-1194369059.1678175243#gs.rc85v1",
+        number: "55471718",
+        from: "AI Singapore",
+        name: "AI FOR INDUSTRY® - Foundations in AI",
+        date: "July 2022",
+        photo: AISGImage
     },
 ]
