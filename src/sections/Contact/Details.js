1239e724fc019ea947e8ba5fc2a9ed339c927727
ryantianj <ryantian64@gmail.com>
2023-04-11 12:30:06 +0800

    update email icon

--- src/sections/Contact/Details.js
+++ src/sections/Contact/Details.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Details.css"
 import {email, location} from "../../Information/Contact";
-import {AiTwotoneMail} from "react-icons/ai";
+import {MdEmail} from "react-icons/md";
 import {ImLocation} from "react-icons/im";
 
 const Details = () => {
     return (
         <div className="detailsWrapper">
             <div className="detailWrapper">
-                <AiTwotoneMail style={{fontSize : "20px"}}/>
+                <MdEmail style={{fontSize : "20px"}}/>
                 <p>{email}</p>
             </div>
             <div className="detailWrapper">
